4b2874f5f45f75eb0203525fc16e1e38395e3d3c
Miguel Prada <mapra99@gmail.com>
2021-04-02 21:08:12 -0500

    active storage setup. Recipe image attachments

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :author
   belongs_to :category
   has_many :ingredients_packs
+
+  has_one_attached :image
+
+  def image_url
+    image.attached? ? image.service_url : ''
+  end
 end
